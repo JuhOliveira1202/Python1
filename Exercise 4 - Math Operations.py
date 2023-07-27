3090c1279af5e5909d99f57b8134ba0f20efd75b
Juliana Oliveira <134332673+JuhOliveira1202@users.noreply.github.com>
2023-07-27 16:14:39 +0100

    Add files via upload

---
+++ Exercise 4 - Math Operations.py
@@ -0,0 +1,27 @@
+#Exercício 4:
+#Elabore um programa que calcule
+
+#4+3(15-2)
+calculo1 = 4+3* (15-2)
+print("4+3* (15-2) = ", calculo1)
+
+#2^3
+calculo2 = 2^3
+print("2^3 =", calculo2)
+
+#40 a dividir por 9
+calculo3 = 40/9 
+print("40/9 =", calculo3)
+
+#resto de divisão 40 por 3
+calculo4 = 40%3
+print("40%3 =", calculo4)
+
+#Divisao inteira de 30 por 4
+calculo5 = 30//4
+print("30//4 =", calculo5)
+
+
+
+
+
